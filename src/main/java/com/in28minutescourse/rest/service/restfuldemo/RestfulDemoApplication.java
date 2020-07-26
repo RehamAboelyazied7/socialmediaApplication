133fac2c7620b7ad8dcb6d6e836c30b62b695590
RehamAboelyazied7 <rehamaboelyazied@gmail.com>
2020-07-26 10:11:15 +0200

    initial commit

---
+++ src/main/java/com/in28minutescourse/rest/service/restfuldemo/RestfulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutescourse.rest.service.restfuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutescourse/rest/service/restfuldemo/RestfulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutescourse.rest.service.restfuldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
