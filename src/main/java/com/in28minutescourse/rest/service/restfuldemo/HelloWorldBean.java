d0eff7fa1116f5f7e324744001dcaf9e16b3deac
RehamAboelyazied7 <rehamaboelyazied@gmail.com>
2020-08-04 08:57:17 +0200

    created hello world bean

---
+++ src/main/java/com/in28minutescourse/rest/service/restfuldemo/HelloWorldBean.java
@@ -0,0 +1,10 @@
+package com.in28minutescourse.rest.service.restfuldemo;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+}
