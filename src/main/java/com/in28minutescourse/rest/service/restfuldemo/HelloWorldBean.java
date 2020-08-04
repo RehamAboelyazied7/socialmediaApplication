c223e1df6889987b3d6bd9540f45364fbc2e86cd
RehamAboelyazied7 <rehamaboelyazied@gmail.com>
2020-08-04 09:01:35 +0200

    created hello world bean getter and setter for exception "converter.HttpMessageNotWritableException: No converter found for return value of type"

--- src/main/java/com/in28minutescourse/rest/service/restfuldemo/HelloWorldBean.java
+++ src/main/java/com/in28minutescourse/rest/service/restfuldemo/HelloWorldBean.java
@@ -7,4 +7,12 @@ public class HelloWorldBean {
     public HelloWorldBean(String message) {
         this.message = message;
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
